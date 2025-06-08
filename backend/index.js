87a285433cb6e6662ead257090d32bc859b6686b
satyam23 <satyampandey2634@gmail.com>
2025-06-08 18:40:51 +0530

    Adding frontend url

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'Cache-Control'],
 }));
 
 
